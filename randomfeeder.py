9bc2e58cbf065b24ff74934e4a6e730a577cd2c3
thom lake <thom.l.lake@gmail.com>
2014-04-22 21:03:14 -0400

    added more options for customization

--- randomfeeder.py
+++ randomfeeder.py
@@ -19,19 +19,20 @@ numlines = [3, 2, 1]
 #numlines = [3, 2, 1, 5, 2, 2, 3]
 #numlines = [3, 2, 1, 5, 2, 7, 11, 9]
 #numlines = [5]
+PLOT_MARKER = '>>'
+ROW_SEP = ';'
+COL_SEP = ' '
+
 
 def randstr():
     return ''.join([choice(printable) for i in range(randint(5, 20))])
 
 data = [[gauss(0, 1) for i in range(num)] for num in numlines]
-print '>>', '; '.join(' '.join(str(x) for x in row) for row in data)
+print PLOT_MARKER, ROW_SEP.join(COL_SEP.join(str(x) for x in row) for row in data)
 
 #while True:
 for i in range(100):
     data = [[gauss(0, 1) + x for x in row] for row in data]
     if random() < 0.1: 
         print randstr()
-    print '>>', '; '.join(' '.join(str(x) for x in row) for row in data)
-    
-
-
+    print PLOT_MARKER, ROW_SEP.join(COL_SEP.join(str(x) for x in row) for row in data) 
--- streamplot.py
+++ streamplot.py
@@ -30,66 +30,110 @@ def get_dimensions(n):
         w += 1
     return h, w
 
+class Subplot(object):
+    """Class for storing subplot configuration and data."""
+    def __init__(self, size, labels=None, colors=None):
+        self.size = size
+        self.labels = labels
+        if colors is None:
+            self.colors = random.sample(COLORS, size)
+        else:
+            self.colors = colors
+        self.xss = [[] for i in range(size)]
+        self.yss = [[] for i in range(size)]
+        self.time = 0
+
+    def append(self, data):
+        for xs, ys, y in zip(self.xss, self.yss, data):
+            xs.append(self.time)
+            ys.append(y)
+        self.time += 1
+
+    def draw(self, ax):
+        for i, (xs, ys, color) in enumerate(zip(self.xss, self.yss, self.colors)):
+            try:
+                label = self.labels[i]
+            except (IndexError, TypeError):
+                ax.plot(xs, ys, color=color)
+            else:
+                ax.plot(xs, ys, color=color, label=label)
+                self.labels[i] = '_nolegend_'
+                ax.legend(loc='best')
+
 class StreamPlotter(object):
-    """Class for holding data and plotting"""
-    def __init__(self, plot_sizes):
-        self.plot_sizes = plot_sizes
-        self.num_plots = len(plot_sizes)
-        self.dataseqs = [[[] for j in range(size)] for size in plot_sizes]
-        self.colormat = [random.sample(COLORS, size) for size in plot_sizes]
+    """Class for representing the entire plot."""
+    def __init__(self, subplots):
+        self.numplots = len(subplots)
+        self.subplots = subplots
         # initialize plot
         fig = plt.figure()
-        h, w = get_dimensions(self.num_plots)
+        h, w = get_dimensions(self.numplots)
         self.axs = [fig.add_subplot(h, w, i) for i in range(1, h * w + 1)]
         # turn on interactive mode and show the plot
         plt.ion()
         plt.show()
-
+    
     def append(self, mat):
-        """Add data to the plots. mat is assumed to
+        """Add data to the subplots. mat is assumed to
         be a jagged iterable of iterables. The jth element
         of the ith iterable is added to the 
-        jth line of the ith plot.
+        jth line of the ith subplot.
         """
-        for subseqs, row in zip(self.dataseqs, mat):
-            for seq, x in zip(subseqs, row):
-                seq.append(x)
+        for subplot, row in zip(self.subplots, mat):
+            subplot.append(row)
 
     def draw(self):
-        for subseqs, colors, ax in zip(self.dataseqs, self.colormat, self.axs):
-            for seq, c in zip(subseqs, colors):
-                ax.plot(seq, color=c)
+        for subplot, ax in zip(self.subplots, self.axs):
+            subplot.draw(ax)
         plt.draw()
 
-def parseline(line):
+def matsplit(string, rowsep=';', colsep=' ', f=None):
+    if f is None:
+        return [[x.strip() for x in row.strip().split(colsep)] 
+                           for row in string.strip().split(rowsep)]
+    return [[f(x.strip()) for x in row.strip().split(colsep)] 
+                          for row in string.strip().split(rowsep)]
+
+def parseline(line, rowsep=';', colsep=' '):
     try:
-        return [[float(x) for x in sub.strip().split(' ')] for sub in line.split(';')]
+        return matsplit(line, rowsep, colsep, f=float)
     except ValueError:
         return []
 
-def streamplot(f, pmark):
+def streamplot(stream, plotmarker, rowsep, colsep, labelmat=None, colormat=None):
     # priming read
     primed = False
     while not primed:
-        line = f.readline()
-        if line.startswith(pmark):
-            data = parseline(line[len(pmark):])
+        line = stream.readline()
+        if line.startswith(plotmarker):
+            data = parseline(line[len(plotmarker):], rowsep, colsep)
             if not len(data):
                 print line
             else:
-                plot_sizes = [len(row) for row in data]
-                plotter = StreamPlotter(plot_sizes)
+                subplots = []
+                for i, row in enumerate(data):
+                    try:
+                        labels = labelmat[i]
+                    except (IndexError, TypeError):
+                        labels = None
+                    try:
+                        colors = colormat[i]
+                    except (IndexError, TypeError):
+                        colors = None
+                    subplot = Subplot(len(row), labels, colors)
+                    subplots.append(subplot)
+                plotter = StreamPlotter(subplots)
                 plotter.append(data)
                 plotter.draw()
                 primed = True
         else:
             print line,
     # read data until eof
-    line = f.readline()
+    line = stream.readline()
     while line:
         line = line.strip()
-        if line.startswith(pmark):
-            data = parseline(line[len(pmark):])
+        if line.startswith(plotmarker):
+            data = parseline(line[len(plotmarker):], rowsep, colsep)
             if not len(data): 
                 print line
             else:
@@ -97,17 +141,31 @@ def streamplot(f, pmark):
                 plotter.draw()
         else:
             print line
-        line = f.readline()
+        line = stream.readline()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('output', nargs='?', 
-                        help='if given, write final image to the given output file')
+                        help='output file name')
     parser.add_argument('-p', '--plot-marker', default='>>', 
-                        help='string indicating line contains data to plot')
+                        help='string indicating line contains data to plot. Defaults to ">>"')
+    parser.add_argument('-R', '--row-sep', default=';',
+                        help='delimits data for different subplots. Defaults to ";"')
+    parser.add_argument('-C', '--col-sep', default=' ',
+                        help='delimits data for the same subplot. Defaults to " "')
+    parser.add_argument('-l', '--labels', default=None, 
+                        help='string of labels. \
+                              " " delimits labels for the same subplot. \
+                              ";" delimits lists of subplot labels \
+                              example: "a1 a2; b1 b2 b3"')
+    parser.add_argument('-c', '--colors', default=None, 
+                        help='string of colors using the same format as labels.\
+                              Can be any valid matplotlib color')
     args = parser.parse_args()
+    labelmat = None if args.labels is None else matsplit(args.labels)
+    colormat = None if args.colors is None else matsplit(args.colors)
     try:
-        streamplot(sys.stdin, args.plot_marker)
+        streamplot(sys.stdin, args.plot_marker, args.row_sep, args.col_sep, labelmat, colormat)
     except KeyboardInterrupt:
         pass
     if args.output is not None:
